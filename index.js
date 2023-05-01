3ab599f4d402f0a1150758e4981186f01a0d6ba8
Vikas preet Tiwana <vikastiwana8@gmail.com>
2023-04-30 20:30:21 -0700

    lab done

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
 // Your code here
+// Move dodger left function
+const dodger = document.getElementById("dodger");
+function moveDodgerLeft() {
+  const leftNumbers = dodger.style.left.replace("px", "");
+  const left = parseInt(leftNumbers, 10);
+
+  if (left > 0) {
+    dodger.style.left = `${left - 1}px`;
+  }
+}
+document.addEventListener("keydown", function (e) {
+  if (e.key === "ArrowLeft") {
+    moveDodgerLeft();
+  }
+});
+
+//Move dodger right function
+
+function moveDodgerRight() {
+  const leftNumbers = dodger.style.left.replace("px", "");
+  const left = parseInt(leftNumbers, 10);
+
+  if (left < 360) {
+    dodger.style.left = `${left + 1}px`;
+  }
+}
+document.addEventListener("keydown", function (e) {
+  if (e.key === "ArrowRight") {
+    moveDodgerRight();
+  }
+});
